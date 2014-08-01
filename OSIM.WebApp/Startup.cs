4b0408618822e4d4a11127233b68436ad49a711f
iplusplus812 <iplusplus@yeah.net>
2014-08-01 21:23:48 +0800

    init projects structure

---
+++ OSIM.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OSIM.WebApp.Startup))]
+namespace OSIM.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
