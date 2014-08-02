59a347cf7044760ecbc21c6e127c208e771efe45
iplusplus812 <iplusplus@yeah.net>
2014-08-02 13:24:08 +0800

    first test for itemtype

---
+++ OSIM.UnitTest/OSIM.Core/ItemRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace OSIM.UnitTest.OSIM.Core
+{
+    public class when_working_with_the_item_type_reository : Specification
+    {
+
+    }
+
+    public class and_saving_a_valid_item_type : when_working_with_the_item_type_reository
+    {
+        private object _result;
+        private IItemTypeRepository _itemTypeRepository;
+        private ItemType _testItemType;
+
+        protected override void Because_of()
+        {
+            _result = _itemTypeRepository.Save(_testItemType);
+        }
+
+        [Test]
+        public void then_a_valid_item_type_id_should_be_returned()
+        {
+           
+        }
+    }
+}
